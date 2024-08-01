2e44305fa649403ad34e48be54115d824f9df0fa
kim-jingyu <swc05161@gmail.com>
2024-08-01 19:47:29 +0900

    feat: MainPage location, back, floor selection modal

---
+++ app/src/main/java/com/knowway/ui/fragment/MainMapFragment.kt
@@ -0,0 +1,18 @@
+package com.knowway.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.knowway.R
+
+class MainMapFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_map, container, false)
+    }
+}
