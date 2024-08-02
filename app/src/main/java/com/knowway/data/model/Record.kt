cf82d0d076692ae978ac4d0b2f8ee1b676c26a20
yjpark <ox8764@naver.com>
2024-08-02 10:45:08 +0900

    feat: 슬라이딩 패널 추가

---
+++ app/src/main/java/com/knowway/data/model/Record.kt
@@ -0,0 +1,10 @@
+package com.knowway.data.model
+
+data class Record(
+    val memberId: Long,
+    val departmentStoreFloorId: Long,
+    val departmentStoreId: Long,
+    val recordTitle: String,
+    val recordLatitude: String,
+    val recordLongitude: String
+)
